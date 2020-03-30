67aeac359793ea7361679b6af545c27f1d0c3552
djfreeart81 <guillaume.gonnet660@gmail.com>
2020-03-30 08:04:54 -0400

    Create MainActivity

--- app/src/main/java/com/guillaumegonnet/shoppinglist022/MainActivity.java
+++ app/src/main/java/com/guillaumegonnet/shoppinglist022/MainActivity.java
@@ -1,14 +1,25 @@
 package com.guillaumegonnet.shoppinglist022;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_CODE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void AddItem(View view) {
+
+        Intent intent = new Intent(this,ShoppingItem.class);
+        startActivityForResult(intent,REQUEST_CODE);
     }
 }
