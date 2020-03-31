2410441c12b9ee4eff849403dfd59ddc6559d189
djfreeart81 <guillaume.gonnet660@gmail.com>
2020-03-30 23:24:29 -0400

    Application full update
    Working, just missing saveInstance

--- app/src/main/java/com/guillaumegonnet/shoppinglist022/MainActivity.java
+++ app/src/main/java/com/guillaumegonnet/shoppinglist022/MainActivity.java
@@ -3,23 +3,62 @@ package com.guillaumegonnet.shoppinglist022;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_CODE = 1;
+    /*
+
+     */
+    int viewCount = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
     }
 
     public void AddItem(View view) {
 
-        Intent intent = new Intent(this,ShoppingItem.class);
-        startActivityForResult(intent,REQUEST_CODE);
+        Intent intent = new Intent(this, ShoppingItem.class);
+        if (viewCount < 10) {
+        startActivityForResult(intent, REQUEST_CODE);
+    } else {
+            Button button = findViewById(R.id.button_main);
+            button.setEnabled(false);
+        }
     }
-}
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+            TextView[] viewList = new TextView[]{findViewById(R.id.item1_main),
+                findViewById(R.id.item2_main),
+                findViewById(R.id.item3_main),
+                findViewById(R.id.item4_main),
+                findViewById(R.id.item5_main),
+                findViewById(R.id.item6_main),
+                findViewById(R.id.item7_main),
+                findViewById(R.id.item8_main),
+                findViewById(R.id.item9_main),
+                findViewById(R.id.item10_main)};
+
+
+            if (REQUEST_CODE == requestCode) {
+                if (RESULT_OK == resultCode) {
+                    String itemToBeAdded = data.getStringExtra(ShoppingItem.SHOPPING_ITEM);
+                    TextView view = viewList[viewCount];
+                    view.setText(itemToBeAdded);
+                    viewCount++;
+                }
+            }
+    }
+} //TODO: saved instance when rotate screen
--- app/src/main/java/com/guillaumegonnet/shoppinglist022/ShoppingItem.java
+++ app/src/main/java/com/guillaumegonnet/shoppinglist022/ShoppingItem.java
@@ -1,14 +1,67 @@
 package com.guillaumegonnet.shoppinglist022;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ShoppingItem extends AppCompatActivity {
 
+    public static final String LOG_TAG = ShoppingItem.class.getSimpleName();
+    public static final String SHOPPING_ITEM = "com.guillaumegonnet.twoactivities.extra.ITEMSELECTED";
+    private String itemSelected;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_item);
+    }
+
+    public void AddItemToShoppingList(View view) {
+        switch (view.getId()) {
+            case R.id.button1_shoppingitem:
+                itemSelected= getString(R.string.item1_shopping_item);
+                break;
+
+            case R.id.button2_shoppingitem:
+                itemSelected=getString(R.string.item2_shopping_item);
+                break;
+            case R.id.button3_shoppingitem:
+                itemSelected=getString(R.string.item3_shopping_item);
+                break;
+            case R.id.button4_shoppingitem:
+                itemSelected=getString(R.string.item4_shopping_item);
+                break;
+            case R.id.button5_shoppingitem:
+                itemSelected=getString(R.string.item5_shopping_item);
+                break;
+            case R.id.button6_shoppingitem:
+                itemSelected=getString(R.string.item6_shopping_item);
+                break;
+            case R.id.button7_shoppingitem:
+                itemSelected=getString(R.string.item7_shopping_item);
+                break;
+            case R.id.button8_shoppingitem:
+                itemSelected=getString(R.string.item8_shopping_item);
+                break;
+            case R.id.button9_shoppingitem:
+                itemSelected=getString(R.string.item9_shopping_item);
+                break;
+            case R.id.button10_shoppingitem:
+                itemSelected=getString(R.string.item10_shopping_item);
+                break;
+            case R.id.button11_shoppingitem:
+                itemSelected=getString(R.string.item11_shopping_item);
+                break;
+        }
+        Log.d(LOG_TAG,"itemSelected is "+itemSelected);
+
+        Intent returnIntent = new Intent();
+        returnIntent.putExtra(SHOPPING_ITEM,itemSelected);
+        setResult(RESULT_OK,returnIntent);
+        finish();
+
     }
 }
